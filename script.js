778db4684f7d6003369e45eb0f2934f740f4e8b0
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-08-17 19:55:14 -0400

    🔧 Add netlify config

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const blackKeys = document.querySelectorAll(".black .key:not(.blank)");
 const whiteKeys = document.querySelectorAll(".white .key");
 
+const pianoKeys = {
+  black: blackKeys,
+  white: whiteKeys
+}
+
 const numberToKeyMap = {
   black: [ "C_sharp", "Eb", "F_sharp", "G_sharp", "Bb", "C_sharp", "Eb", "F_sharp", "G_sharp", "Bb", ],
   white: [ "C", "D", "E", "F", "G", "A", "B", "C", "D", "E", "F", "G", "A", "B", ]
@@ -11,16 +16,15 @@ function playKey(filename) {
   audio.play();
 }
 
-function init() {
-  blackKeys.forEach((key, i) => {
-    const filename = `${numberToKeyMap.black[i]}.mp3`
-    key.addEventListener("click", () => playKey(filename))
-  });
-
-  whiteKeys.forEach((key, i) => {
-    const filename = `${numberToKeyMap.white[i]}.mp3`
-    key.addEventListener("click", () => playKey(filename))
+function addListeners() {
+  Object.keys(pianoKeys).forEach((color) => {
+    pianoKeys[color].forEach((key, i) => {
+      const filename = `${numberToKeyMap[color][i]}.mp3`
+      key.addEventListener("click", () => playKey(filename))
+    })
   });
 }
 
-init();
+(function init() {
+  addListeners();
+})()
