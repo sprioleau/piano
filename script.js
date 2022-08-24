73d1e7760e3a7f3f0a99f6813a30a85cbd589d43
San'Quan Prioleau <sq.prioleau@gmail.com>
2022-08-23 23:13:50 -0500

    ✨ Add hand illustration

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let hueRotate = 300;
 let mouseIsDown = false;
 
+const root = document.querySelector(":root");
+const hand = document.querySelector(".hand");
+const vw = (window.innerWidth / 100);
+const vh = (window.innerHeight / 100);
+const handFadeOffset = getComputedStyle(root).getPropertyValue("--hand-size").split("vw")[0] / 2;
+
 const pianoKeys = {
   black: document.querySelectorAll(".black .key:not(.blank)"),
   white: document.querySelectorAll(".white .key"),
@@ -40,6 +46,23 @@ function addListeners() {
       key.addEventListener("mouseup", () => {
         mouseIsDown = false;
       });
+
+      window.addEventListener("mousemove", (event) => {
+        const { clientX: x, clientY: y } = event;
+
+        const isOutsideXBoundary = x < vw * handFadeOffset || x > (100 - handFadeOffset) * vw;
+        console.log('handFadeOffset:', handFadeOffset)
+        const isOutsideYBoundary = y < vh * handFadeOffset || y > (100 - handFadeOffset) * vh;
+        const shouldHideHand = isOutsideXBoundary || isOutsideYBoundary;
+        console.log('shouldHideHand:', shouldHideHand)
+        
+        shouldHideHand
+          ? hand.classList.add("hide")
+          : hand.classList.remove("hide");
+
+        root.style.setProperty("--hand-x", `${x}px`);
+        root.style.setProperty("--hand-y", `${y}px`);
+      })
     })
   });
 }
